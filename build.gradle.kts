24545d29763cc0f9962069eb1a369fa21f775347
early <early@DESKTOP-46HD39H>
2023-06-24 15:20:38 +0900

    initial configurations

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+	java
+	war
+	id("org.springframework.boot") version "3.1.1"
+	id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "com.earlyou"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.2")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+	annotationProcessor("org.projectlombok:lombok")
+	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:3.0.2")
+	
+	implementation("javax.inject:javax.inject:1")
+	implementation("org.apache.tomcat.embed:tomcat-embed-jasper:10.1.10")
+	compileOnly("javax.servlet:javax.servlet-api:4.0.1")
+	implementation("javax.servlet:jstl:1.2")
+	implementation("com.googlecode.json-simple:json-simple:1.1.1")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/com/earlyou/aom/AomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earlyou.aom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
