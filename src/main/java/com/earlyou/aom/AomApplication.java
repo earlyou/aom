3eb77028ba9ca4e26b31acebe69d0176e2e3360c
earlyou <earlyou1@gmail.com>
2023-06-29 14:59:29 +0900

    reset

---
+++ src/main/java/com/earlyou/aom/AomApplication.java
@@ -0,0 +1,13 @@
+package com.earlyou.aom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AomApplication.class, args);
+	}
+
+}
