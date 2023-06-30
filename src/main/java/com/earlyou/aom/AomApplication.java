f8e0644100ba28746e44d4c6803fd823c81c6d66
earlyou <earlyou1@gmail.com>
2023-06-30 21:58:28 +0900

    main application class extends SpringBootServletInitializer

--- src/main/java/com/earlyou/aom/AomApplication.java
+++ src/main/java/com/earlyou/aom/AomApplication.java
@@ -2,12 +2,20 @@ package com.earlyou.aom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AomApplication {
+public class AomApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
-		SpringApplication.run(AomApplication.class, args);
+		SpringApplication application = new SpringApplication(AomApplication.class);
+		application.run(args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AomApplication.class);
 	}
 
 }
