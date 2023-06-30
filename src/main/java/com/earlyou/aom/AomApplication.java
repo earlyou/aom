53ebedca0696ef4dad8b15af3e44534d30326e35
earlyou <earlyou1@gmail.com>
2023-06-30 22:02:16 +0900

    main application class override SpringApplicationBuilder

--- src/main/java/com/earlyou/aom/AomApplication.java
+++ src/main/java/com/earlyou/aom/AomApplication.java
@@ -9,8 +9,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class AomApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
-		SpringApplication application = new SpringApplication(AomApplication.class);
-		application.run(args);
+		SpringApplication.run(AomApplication.class, args);
 	}
 	
 	@Override
