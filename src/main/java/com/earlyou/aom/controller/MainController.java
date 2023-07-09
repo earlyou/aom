cf14b1a91c2e84fe96aa1aec86582a0fef0f752e
earlyou <earlyou1@gmail.com>
2023-07-09 16:05:45 +0900

    system specification

--- src/main/java/com/earlyou/aom/controller/MainController.java
+++ src/main/java/com/earlyou/aom/controller/MainController.java
@@ -1,11 +1,40 @@
 package com.earlyou.aom.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.earlyou.aom.biz.CpuBiz;
+import com.earlyou.aom.biz.FilestoreBiz;
+import com.earlyou.aom.biz.MbBiz;
+import com.earlyou.aom.biz.OsinfoBiz;
+import com.earlyou.aom.biz.RamBiz;
+import com.earlyou.aom.biz.VgaBiz;
+import com.earlyou.aom.vo.CpuVO;
+import com.earlyou.aom.vo.FilestoreVO;
+import com.earlyou.aom.vo.MbVO;
+import com.earlyou.aom.vo.OsinfoVO;
+import com.earlyou.aom.vo.RamVO;
+import com.earlyou.aom.vo.VgaVO;
+
 @Controller
 public class MainController {
+	
+	@Autowired
+	OsinfoBiz osbiz;
+	@Autowired
+	MbBiz mbbiz;
+	@Autowired
+	CpuBiz cpubiz;
+	@Autowired
+	RamBiz rambiz;
+	@Autowired
+	FilestoreBiz fsbiz;
+	@Autowired
+	VgaBiz vgabiz;
 
 	@GetMapping("/")
 	public String main(Model m) {
@@ -16,12 +45,38 @@ public class MainController {
 	
 	@GetMapping("/specification")
 	public String specification(Model m) {
+		m.addAttribute("sidebar", "sidebar");
 		m.addAttribute("main", "sysspec/specification");
+
+		OsinfoVO os = null;
+		MbVO mb = null;
+		CpuVO cpu = null;
+		List<RamVO> ramlist = null;
+		List<FilestoreVO> fslist = null;
+		List<VgaVO> vgalist = null;
+		try {
+			os = osbiz.get().get(0);
+			mb = mbbiz.get().get(0);
+			cpu = cpubiz.get().get(0);
+			ramlist = rambiz.get();
+			fslist = fsbiz.get();
+			vgalist = vgabiz.get();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		m.addAttribute("os", os);
+		m.addAttribute("mb", mb);
+		m.addAttribute("cpu", cpu);
+		m.addAttribute("ramlist", ramlist);
+		m.addAttribute("fslist", fslist);
+		m.addAttribute("vgalist", vgalist);
 		return "index";
 	}
 	
 	@GetMapping("/performance")
 	public String performance(Model m) {
+		m.addAttribute("sidebar", "sidebar");
 		m.addAttribute("main", "sysspec/performance");
 		return "index";
 	}
