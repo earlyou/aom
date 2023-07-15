22d3eceb187bd6cfb270137c4f497659873f3dfd
earlyou <earlyou1@gmail.com>
2023-07-15 16:12:23 +0900

    testing ajax

--- src/main/resources/static/assets/static/js/pages/performance.js
+++ src/main/resources/static/assets/static/js/pages/performance.js
@@ -23,76 +23,11 @@ $(document).ready(function() {
 	var cpuusage;
 	setInterval(function() {
 		cpuusage = getcpu();
-		console.log(cpuusage)
+		console.log('getcpu: ' + cpuusage)
 		updateData(CpuChart, cpuusage);
 		CpuChart.update();
 
 	}, 1000);
-
-	/**
-	
-		let optionsCpuUsage = {
-			series: [0, 100],
-			labels: ["Usage", "Free"],
-			colors: ["#435ebe", "#55c6e8"],
-			chart: {
-				type: "donut",
-				width: "100%",
-				height: "350px",
-			},
-			legend: {
-				position: "bottom",
-			},
-			plotOptions: {
-				pie: {
-					donut: {
-						size: "30%",
-					},
-				},
-			},
-		}
-	
-		var cpuUsage = new ApexCharts(
-			document.getElementById("cpu-usage"),
-			optionsCpuUsage
-		)
-		cpuUsage.render();
-	
-		var cpuusage;
-		setInterval(function() {
-			cpuusage = getcpu();
-	
-			optionsCpuUsage = {
-				series: [cpuusage, 100 - cpuusage],
-				labels: ["Usage", "Free"],
-				colors: ["#435ebe", "#55c6e8"],
-				chart: {
-					type: "donut",
-					width: "100%",
-					height: "350px",
-				},
-				legend: {
-					position: "bottom",
-				},
-				plotOptions: {
-					pie: {
-						donut: {
-							size: "30%",
-						},
-					},
-				},
-			}
-	
-	
-			var cpuUsage = new ApexCharts(
-				document.getElementById("cpu-usage"),
-				optionsCpuUsage
-			)
-			$("#cpu-usage").empty();
-			cpuUsage.render();
-	
-		}, 2000);
-	 */
 });
 
 function getcpu() {
@@ -106,6 +41,7 @@ function getcpu() {
 			cpu = data;
 		}
 	});
+	console.log('ajax cpu: ' + cpu)
 	return cpu;
 }
 
